a976a41e3451d61dd93cb292dfcb48dbcca8cc35
Jet Chien <67193495+jet-chien@users.noreply.github.com>
2020-12-04 01:35:26 +0800

    add ourcases

---
+++ Project_2/runOurcases/runOurcase5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ../
+rm -f a.out
+rm -f lex.yy.c
+rm -f toy.tab.c
+rm -f toy.tab.h
+rm -f toy.output
+flex toy.l
+bison -vd toy.y &>/dev/null
+cc lex.yy.c toy.tab.c -lfl
+./a.out < ourcases/ourcase5.toy
